3b6948ea25d7489c4076da20ef06981a8f0d6b2c
Matt M <matthewdsmorgan@gmail.com>
2025-04-13 16:08:39 +0100

    fixed pub year select slider to step between available years

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,12 @@
 import pandas as pd
 import streamlit as st
 
-st.write('initial dashboard')
+st.write('## Current Ideas')
+st.write('- chart with values about what books have been read by me and soph with overlap')
+st.write('- chart for book price')
+st.write('- chart with book page numbers')
+st.write('- chart for publishers, top 5 then others?')
+st.write('- chart for primary genre')
+st.write('- chart for booktype')
+st.write('- chart for pub year')
+
